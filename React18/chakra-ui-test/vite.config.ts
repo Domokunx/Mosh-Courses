a828c6826c97421731081fa0e8d095258d4f6893
Domokunx <cliffkoes@gmail.com>
2023-05-03 00:41:43 +0800

    build

--- React18/chakra-ui-test/vite.config.ts
+++ React18/chakra-ui-test/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   root:"src/pages",
   plugins: [react()],
   build: {
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         layout: resolve(__dirname, "src", "pages", "Layout", "index.html"),
