b4ec06872e5042556576e671dcb6d0e747f92c9d
Domokunx <cliffkoes@gmail.com>
2023-05-23 19:00:56 +0800

    Implement Dark Mode

--- React18/mosh-project/src/components/navbar.tsx
+++ React18/mosh-project/src/components/navbar.tsx
@@ -1,11 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
+import { Button, HStack, Image, Text, useColorMode } from "@chakra-ui/react"
 import logo from "../assets/react.svg"
 
 const Navbar = () => {
+    const { toggleColorMode } = useColorMode();
   return (
     <HStack>
         <Image src={logo} boxSize={"60px"} />
         <Text>NavBar</Text>
+        <Button onClick={toggleColorMode}> Color Scheme</Button>
     </HStack>
   )
 }
