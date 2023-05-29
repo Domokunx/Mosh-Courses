ad23744a3d21684cfbc980909b69e383cf8064f2
Domokunx <cliffkoes@gmail.com>
2023-05-29 20:07:11 +0800

    Playing with Skeleton Component

--- React18/mosh-project/src/components/gamecardskeleton.tsx
+++ React18/mosh-project/src/components/gamecardskeleton.tsx
@@ -1,9 +1,10 @@
-import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { Card, CardBody, Image, SkeletonText } from "@chakra-ui/react";
+import face from "../assets/Screenshot_20230128_113743.png"
 
 const GameCardSkeleton = () => {
   return (
     <Card>
-      <Skeleton height={"200px"} width={"600px"} />
+      <Image src={face} />
       <CardBody>
         <SkeletonText />
       </CardBody>
