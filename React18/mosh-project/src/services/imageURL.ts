4e788c133666040df383b849fb41d6ef74735b04
Domokunx <cliffkoes@gmail.com>
2023-06-06 17:30:12 +0800

    Handle games with no image

--- React18/mosh-project/src/services/imageURL.ts
+++ React18/mosh-project/src/services/imageURL.ts
@@ -1,5 +1,7 @@
+import placeholder from '../assets/Screenshot_20230128_113743.png'
+
 const getCroppedImage = (imageURL: string) =>  {
-    if (!imageURL) return '';
+    if (!imageURL) return placeholder;
     const index = imageURL.indexOf('media/') + 'media/'.length;
     
     return imageURL.slice(0, index) + 'crop/600/400/' + imageURL.slice(index);
